fa7f7126414c1d451537ab6051b33b6f3b1a8997
Blake Elias <blakeelias@gmail.com>
2011-06-14 15:59:53 -0400

    Saved fuel in the square project and prevented from coasting at the end.

--- square/blake_elias_square.c
+++ square/blake_elias_square.c
@@ -6,11 +6,12 @@
 #include "spheres_constants.h"
 static int state;
  
-//User01: stuynaught Team: Stuy-Naught Project: SQUARE
+//User01: blakeelias Team: Stuy-Naught Project: Square
 void ZRUser01(float *myState, float *otherState, float time)
 {
 float P[3];  // The point to move towards in (x, y, z) coordinates
-float dist;  // The distance we are from said point
+float dist;  // The distance we are from said point (calculated)
+float V[3];  // The velocity we should travel at to reach that point (calculated)
  
 if (state == 0) {
     // point (0.5, 0.5, 0), first corner of our square
@@ -34,14 +35,34 @@ else if (state == 3) {
     P[2] = 0.0;
 }
  
-ZRSetPositionTarget(P);  // move towards point P
+//ZRSetPositionTarget(P);  // move towards point P
+ 
+V[0] = P[0] - myState[0];
+V[1] = P[1] - myState[1];
+V[2] = P[2] - myState[2];
+ 
+mathVecNormalize(V, 3);
+V[0] = V[0] * 0.03;
+V[1] = V[1] * 0.03;
+V[2] = V[2] * 0.03;
+ 
+if (getPercentFuelRemaining() < 8) {
+    V[0] = 0;
+    V[1] = 0;
+    V[2] = 0;
+}
+ZRSetVelocityTarget(V);
  
 dist = sqrt((myState[0]-P[0])*(myState[0]-P[0]) +
             (myState[1]-P[1])*(myState[1]-P[1]) +
             (myState[2]-P[2])*(myState[2]-P[2]));  // find our distance from the corner we're traveling to
  
+ 
 if (dist < 0.05) {
     state = state + 1;  // move to the next corner if we're within 5 cm of the current target
+    if (state == 4) {
+        state = 0;
+    }
 }
 }
 void ZRInit01()
